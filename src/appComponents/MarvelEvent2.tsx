6aceead18e06d72d7b58a3cb1a9950ce299a1135
rodolphe <rodolphe@MacBookPro.lan>
2024-10-01 11:43:31 +0200

    added spiderman card for example

---
+++ src/appComponents/MarvelEvent2.tsx
@@ -0,0 +1,29 @@
+import "./marvel-event.css";
+
+const MarvelEvent2 = () => {
+  return (
+    <div>
+      <div className="card-marvel" id="spiderman">
+        <div className="card-marvel-image">
+          <img src="https://pngimg.com/uploads/spider_man/spider_man_PNG59.png" />
+        </div>
+        <div className="card-marvel-text">
+          <h2>MARVEL</h2>
+
+          <div>
+            <div>
+              <span>COMICS</span>
+              <span>GROUP</span>
+            </div>
+            <div>
+              <span>12</span>
+              <span>&euro;</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarvelEvent2;
--- src/dataCards.tsx
+++ src/dataCards.tsx
@@ -4,6 +4,7 @@ import Card3d2 from "./appComponents/Card3d2";
 import Card3d1 from "./appComponents/Card3d1";
 import FoodCard from "./appComponents/FoodCard";
 import MarvelEvent1 from "./appComponents/MarvelEvent1";
+import MarvelEvent2 from "./appComponents/MarvelEvent2";
 
 export const contentForModal = [
     {
@@ -167,7 +168,7 @@ export const contentForModal = [
           <h1 style={{ fontWeight: "bold", padding: 10 }}>
             session de discution sur les revues Marvel
           </h1>
-          <MarvelEvent1 />
+          <MarvelEvent2 />
         </Fragment>
       ),
     },
